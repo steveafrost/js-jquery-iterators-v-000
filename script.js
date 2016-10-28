773c9f2986bb66ecd8ed7a6e3576052a439115c5
steveafrost <ffrostt@gmail.com>
2016-10-27 23:39:19 -0400

    learned about jQuery iterators

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+var names = ["Carleton", "Avi", "Azat", "Ann"];
+
 $(document).ready(function(){
 
+$.each(names, function(index, name){
+  console.log("hey " + name);
+});
+
+$.map(names, function(name, index){
+   return name + " loves JavaScript";
+});
 
 });
